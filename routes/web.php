1f0390b4fa98dbb3b94f02d78c61c750c63ddba9
Tisha Khathurani <tishaak17@gmail.com>
2026-02-01 20:29:41 +0530

    feat: added a route to verify dashboard page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/job-monitor', function () {
+    return response()->file(
+        public_path('job-monitor/index.html')
+    );
+});
