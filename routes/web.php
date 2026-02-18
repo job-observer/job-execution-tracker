01c149413ef320cbd0332b02a1064a6826b7128b
Tisha Khathurani <tishaak17@gmail.com>
2026-02-18 15:21:47 +0530

    feat: modified routes to get metrics and display job data

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JobMonitorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/job-monitor', function () {
-    return response()->file(
-        public_path('job-monitor/index.html')
-    );
-});
+
+Route::get('/dashboard/metrics', [DashboardController::class, 'metrics']);
+
+Route::get('/monitor', [JobMonitorController::class, 'index']);
+
